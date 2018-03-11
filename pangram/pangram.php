c9d850e49bb28ff5c2c29625f3f0f77816e22e03
Francisco Camargos <kxdevel@gmail.com>
2018-03-11 10:26:45 +0000

    Pangram

---
+++ pangram/pangram.php
@@ -0,0 +1,21 @@
+<?php
+
+function isPangram($word) {
+	$word = clean_word($word);
+	
+	if(count(unique_letters($word)) == 26) {
+		return true;
+	}
+
+	return false;
+}
+
+function clean_word($word) {
+	$word = strtolower($word);
+	return preg_replace('#[ \\\]#', '', $word);
+}
+
+function unique_letters($word) {
+	preg_match_all('/[a-z]/', $word, $matches);
+	return array_unique($matches[0]);
+}
